1c2383708e10bb1d46601e9dfcf59c3b2e9d0f76
pedro-pablo <pedropablopolanco@hotmail.com>
2019-05-23 23:00:28 -0400

    Added search input treatment for URIs

--- js/main.js
+++ js/main.js
@@ -3,11 +3,17 @@
  */
 const clockInterval = 100;
 
+/**
+ * Search engine query url
+ */
+const searchEngineUrl = 'https://duckduckgo.com/?q=';
+
 const tabKeyCode = 9;
 const enterKeyCode = 13;
 const escapeKeyCode = 27;
 const searchBarElement = document.getElementById('search-bar');
 const clockElement = document.getElementById('clock');
+const formElement = document.getElementById('search-form');
 
 /**
  * Return a string containing the formatted current date and time.
@@ -19,7 +25,7 @@ function getDateTime() {
     let hour = dateTime.getHours();
     let minutes = dateTime.getMinutes();
     let seconds = dateTime.getSeconds();
-    
+
     if (hour < 0) {
         hour = 24 + hour;
     }
@@ -34,6 +40,19 @@ function setClock() {
     clockElement.innerText = getDateTime();
 }
 
+function search() {
+    let value = searchBarElement.value;
+    if (!value) {
+        return;
+    }
+
+    if (value.startsWith('https://') || value.startsWith('http://')) {
+        window.location = value;
+    } else {
+        window.location = searchEngineUrl + encodeURIComponent(value);
+    }
+}
+
 
 setClock();
 
@@ -44,7 +63,12 @@ setInterval(() => {
 searchBarElement.focus();
 searchBarElement.value = '';
 
-document.addEventListener('keydown', (event) => {
+formElement.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    search();
+});
+
+document.addEventListener('keypress', (event) => {
     if (event.keyCode == escapeKeyCode) {
         searchBarElement.blur();
         searchBarElement.value = '';
